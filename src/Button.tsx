8e0f303289ce134d1576416d30771fce7f53baac
SAWADA Takayoshi <sawadasuiren@gmail.com>
2020-07-12 09:56:17 +0900

    feat: Add esm build settings

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-primitives';
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
 
 const style = StyleSheet.create({
   wrapper: {
     backgroundColor: 'red',
   }
-});
+})
 
-const Button = () => (
+const Button: React.FC = () => (
   <View style={style.wrapper}>
     <Text>Button</Text>
   </View>
-);
+)
 
-export default Button;
+export default Button
